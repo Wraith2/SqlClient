881cbc84f31429b8723ce6f232b2053f87dc07aa
Wraith2 <wraith2@gmail.com>
2025-08-04 01:08:14 +0100

    xml fix

--- src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SqlCachedBuffer.cs
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SqlCachedBuffer.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.Collections.Generic;
 using System.Data.SqlTypes;
 using System.Diagnostics;
@@ -45,10 +46,6 @@ namespace Microsoft.Data.SqlClient
             if (canContinue && isContinuing)
             {
                 cachedBytes = stateObj.TryTakeSnapshotStorage() as List<byte[]>;
-                if (isStarting)
-                {
-                    cachedBytes = null;
-                }
             }
  
             if (cachedBytes == null)
